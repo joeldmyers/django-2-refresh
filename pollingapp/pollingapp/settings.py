fdb91c9c50fd01e803343d9cb289d47e1556c69b
Joel Myers <joeldmyers@gmail.com>
2019-10-21 22:34:53 -0400

    heroku setup

--- pollingapp/pollingapp/settings.py
+++ pollingapp/pollingapp/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-import django-heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -122,4 +121,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Activate Django-Heroku.
-django-heroku.settings(locals())
+import django_heroku
+django_heroku.settings(locals())
